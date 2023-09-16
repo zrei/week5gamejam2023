2bddebc13a7fad58d96092064904144f629a9f55
zrei <88964793+zrei@users.noreply.github.com>
2023-09-16 13:49:08 +0800

    Add number of points in shape to global settings

--- gamejam/Assets/Resources/Scripts/GlobalSettings.cs
+++ gamejam/Assets/Resources/Scripts/GlobalSettings.cs
@@ -5,4 +5,8 @@ public class GlobalSettings : Singleton<GlobalSettings>
     [Header("Debug")]
     [SerializeField] private bool m_DebugTextOn = false;
     public static bool g_DebugTextOn => Instance.m_DebugTextOn;
+
+    [Header("Mouse Points")]
+    [SerializeField] private int m_PointsInShape = 4;
+    public static int g_PointsInShape => Instance.m_PointsInShape;
 }
