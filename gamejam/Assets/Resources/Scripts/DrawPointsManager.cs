fce8c64775229723788c2d0c3c2069d681ce09bd
zrei <88964793+zrei@users.noreply.github.com>
2023-09-16 16:28:02 +0800

    Valid polygon checking

--- gamejam/Assets/Resources/Scripts/DrawPointsManager.cs
+++ gamejam/Assets/Resources/Scripts/DrawPointsManager.cs
@@ -6,19 +6,20 @@ public class DrawPointsManager : Singleton<DrawPointsManager>
 {
     //[SerializeField] private Canvas m_Canvas;
     [SerializeField] private GameObject m_PointObject;
-
     private List<Vector3> m_PointsInShape = new List<Vector3>(); 
+
     private List<GameObject> m_PointObjects;
+    private LineRenderer m_LineRenderer;
 
     protected override void HandleAwake()
     {
         base.HandleAwake();
         GlobalEvents.CursorEvents.DrawPointEvent += DrawPoints;
-        
+        m_LineRenderer = GetComponent<LineRenderer>();
         if (GlobalSettings.IsReady)
-            InitialisePoints();
+            HandleDependencies();
         else
-            GlobalSettings.OnReady += InitialisePoints;
+            GlobalSettings.OnReady += HandleDependencies;
     }
 
     protected override void HandleDestroy()
@@ -27,9 +28,9 @@ public class DrawPointsManager : Singleton<DrawPointsManager>
         GlobalEvents.CursorEvents.DrawPointEvent -= DrawPoints;
     }
 
-    private void InitialisePoints()
+    private void HandleDependencies()
     {
-        GlobalSettings.OnReady -= InitialisePoints;
+        GlobalSettings.OnReady -= HandleDependencies;
         m_PointObjects = new List<GameObject>(GlobalSettings.g_PointsInShape);
         for (int i = 0; i < GlobalSettings.g_PointsInShape; i++)
         {
@@ -37,6 +38,7 @@ public class DrawPointsManager : Singleton<DrawPointsManager>
             newPoint.SetActive(false);
             m_PointObjects.Add(newPoint);
         }
+        m_LineRenderer.positionCount = 0;
     }
 
     // returns disabled game object
@@ -48,18 +50,43 @@ public class DrawPointsManager : Singleton<DrawPointsManager>
         return null;
     }
 
+    private void DisableAllPoints()
+    {
+        foreach (GameObject point in m_PointObjects)
+            point.SetActive(false);
+    }
+
     private void DrawPoints(Vector3 mousePosition)
     {
         if (GlobalSettings.g_DebugTextOn)
             Debug.Log("Mouse coordinates: " + mousePosition);
-        // distance check here, but I'll do it later
+        
+        // draw point
         GameObject point = GetPoint();
         Vector3 pos = Camera.main.ScreenToWorldPoint(mousePosition);//new Vector3(mousePosition.x, mousePosition.y, 0f);
         point.transform.position = new Vector3(pos.x, pos.y, 0f);
         point.SetActive(true);
-        Debug.Log("???");
-        m_PointsInShape.Add(mousePosition);
+
+        // draw line
+        m_LineRenderer.positionCount += 1;
+        m_LineRenderer.SetPosition(m_PointsInShape.Count, point.transform.position);
+        m_PointsInShape.Add(mousePosition); 
+
+        // reset once you reach four points
         if (m_PointsInShape.Count == GlobalSettings.g_PointsInShape)
+        {
+            m_LineRenderer.positionCount += 1;
+            m_LineRenderer.SetPosition(m_PointsInShape.Count, m_PointsInShape[0]);
+            StartCoroutine(Delay(2f));
+            m_LineRenderer.positionCount = 0;
             m_PointsInShape.Clear();
+            DisableAllPoints();
+            GlobalEvents.CursorEvents.ClearPointsEvent?.Invoke();
+        }
+    }
+
+    private IEnumerator Delay(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
     }
 }
