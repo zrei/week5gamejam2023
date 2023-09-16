422a7ed1f043055f2addea78f5cd02553dd6435d
Domokunx <cliffkoes@gmail.com>
2023-09-16 19:34:20 +0800

    Main Menu

--- gamejam/Assets/Resources/Scripts/MainMenu.cs
+++ gamejam/Assets/Resources/Scripts/MainMenu.cs
@@ -3,20 +3,28 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Button m_Button;
+    [SerializeField] private Button m_StartButton;
+    [SerializeField] private Button m_QuitButton;
 
     private void Awake()
     {
-        m_Button.onClick.AddListener(TransitionToNextScene);
+        m_StartButton.onClick.AddListener(TransitionToNextScene);
+        m_QuitButton.onClick.AddListener(OnQuitGame);
     }
 
     private void OnDestroy()
     {
-        m_Button.onClick.RemoveListener(TransitionToNextScene);
+        m_StartButton.onClick.RemoveListener(TransitionToNextScene);
+        m_QuitButton.onClick.RemoveListener(OnQuitGame);
     }
 
     private void TransitionToNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void OnQuitGame()
+    {
+        Application.Quit();
     }
 }
